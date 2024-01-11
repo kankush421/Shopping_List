2aa90ba533f618255535ca4bc5a91c4c82b298d0
Ankush kumar <kankush421@gmail.com>
2024-01-11 20:05:47 +0000

    added more js code

--- src/main.js
+++ src/main.js
@@ -1,23 +1,74 @@
-const itemForm = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemList = document.getElementById('item-list');
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const itemFilter = document.getElementById("filter");
+const itemClear = document.getElementById("clear");
 
-function addItem(e){
-   e.preventDefault();
+function addItem(e) {
+  e.preventDefault();
 
-   const newItem = itemInput.value;
+  const newItem = itemInput.value;
 
-   // Validate Item
-   if(newItem === ''){
+  // Validate Item
+  if (newItem === "") {
     alert("Add Item First");
     return;
-   }
+  }
 
-   const li
-
+  //Create List Item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  itemList.appendChild(li);
+  checkUI();
 }
 
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
 
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (window.confirm("Are you Sure !! ")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function removeAllItem() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    itemFilter.style.display = "none";
+    itemClear.style.display = "none";
+  } else {
+    itemFilter.style.display = "block";
+    itemClear.style.display = "block";
+  }
+}
 //Event Listeners
 
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+itemClear.addEventListener("click", removeAllItem);
+
+checkUI();
